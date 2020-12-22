6076d2174231ec0797af021d303e8d59148d68d4
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2020-12-22 19:09:21 +0100

    Add project files.

---
+++ PlayApp/PlayApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlayApp
+{
+  
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async Task AlertAsync()
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+
+        }
+        protected void OnCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (((CheckBox)sender).IsChecked)
+            {
+                ((CheckBox)sender).Color = Color.Green;
+                AlertAsync();
+            }
+            else
+            {
+                ((CheckBox)sender).Color = Color.Gray;
+            }
+        }
+
+        Color defaultColor = Color.DarkBlue;
+        protected void OnCheckedChangeBlue(object sender, CheckedChangedEventArgs e)
+        {
+            CheckBox ch = (CheckBox)sender;
+            if (ch.IsChecked)
+            {
+                Frame1.BackgroundColor = Color.Aqua;
+                Label1.TextColor = Color.Black;
+            }
+            else
+            {
+                Frame1.BackgroundColor = defaultColor;
+                Label1.TextColor = Color.White;
+
+            }
+        }
+    }
+}
